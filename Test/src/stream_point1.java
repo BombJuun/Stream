f8a628628fad151fb1f33ed33cc5b3743016ce8a
Bumjun <parkbj1102@naver.com>
2023-09-04 16:30:57 +0900

    init

--- Test/src/stream_point1.java
+++ Test/src/stream_point1.java
@@ -5,10 +5,13 @@ import java.util.stream.Collectors;
 public class stream_point1 {
     public static void main(String[] args) throws Exception {
         List<Integer> list=Arrays.asList(3,1,5,4,2);
+        //정렬 후 다시 리스트로 변환
         List<Integer> sortedList=list.stream().sorted().collect(Collectors.toList());
+        
 
-        System.out.println(list);
-        System.out.println(sortedList);
-        System.out.println();
+
+        System.out.println(list);   //원본 그대로
+        System.out.println(sortedList);//정렬된 리스트
+        
     }
 }
